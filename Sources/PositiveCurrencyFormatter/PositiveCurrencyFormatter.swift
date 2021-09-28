b05f5cbc36018cbd425f18f23ec16c60e4bb74e6
Marat Saytakov <marats@backbase.com>
2021-09-28 19:09:52 +0200

    expose more NumberFormatter's properties & update tests

--- Sources/PositiveCurrencyFormatter/PositiveCurrencyFormatter.swift
+++ Sources/PositiveCurrencyFormatter/PositiveCurrencyFormatter.swift
@@ -3,7 +3,18 @@ import Foundation
 /// A PositiveCurrencyFormatter to format both positive and negative amounts
 /// with a proper placement of plus or minus signs according to currency and/or locale.
 /// Uses Apple's NumberFormatter under the hood.
-public class PositiveCurrencyFormatter: NumberFormatter {
+@available(macOS 10.5, *)
+public final class PositiveCurrencyFormatter {
+
+  // MARK: - Private -
+
+  /// A dash sign (U+002D) to be replace with a minus sign (U+2212).
+  private let dashSign = "-"
+
+  /// Internal instance of CurrencyNumberFormatter.
+  private let currencyFormatter: CurrencyNumberFormatter
+
+  // MARK: - Public -
 
   /// A positive amount sign.
   /// Default value: + PLUS SIGN
@@ -15,22 +26,185 @@ public class PositiveCurrencyFormatter: NumberFormatter {
   /// Unicode: U+2212, UTF-8: E2 88 92
   public var negativeSign = "−"
 
-  /// A dash sign (U+002D) to replace with a minus sign (U+2212).
-  private let dashSign = "-"
-
   /// A PositiveCurrencyFormatter initializer.
+  ///
   /// - Parameters:
   ///   - locale: a locale to use when formatting numbers. Default value: `Locale.current`.
-  ///   - currencyCode: a currency code to use when formatting numbers. Default value: `"USD"`.
-  ///   - currencySymbol: a currency symbol (such as '$') to use when formatting numbers. Default value: `nil`.
-  public convenience init(locale: Locale = .current, currencyCode: String? = "USD", currencySymbol: String? = nil) {
-    self.init()
-    self.locale = locale
-    self.currencyCode = currencyCode
-    self.currencySymbol = currencySymbol
+  ///   - currencyCode: a currency code (such as 'USD') to use when formatting numbers. Default value is `nil`,
+  ///   which means there would be a currency provided by locale.
+  ///   - currencySymbol: a currency symbol (such as '$') to use when formatting numbers. Default value is `nil`,
+  ///   which means there would be a currency sign provided by locale.
+  public init(locale: Locale = .current, currencyCode: String? = nil, currencySymbol: String? = nil) {
+    let formatter = CurrencyNumberFormatter()
+    formatter.locale = locale
+    formatter.currencyCode = currencyCode
+    formatter.currencySymbol = currencySymbol
+    self.currencyFormatter = formatter
   }
 
-  private override init() {
+  /// Returns a string containing the formatted value of the provided number object.
+  ///
+  /// - Parameter number: An NSNumber object that is parsed to create the returned string object.
+  /// - Returns: A string containing the formatted value of number using the receiver’s current settings.
+  public func string(from number: NSNumber) -> String? {
+    if number.doubleValue > 0 {
+      var value = number.doubleValue
+      value.negate()
+      return currencyFormatter.string(from: NSNumber(value: value))?
+        .replacingOccurrences(of: dashSign, with: positiveSign)
+        .replacingOccurrences(of: negativeSign, with: positiveSign)
+    }
+
+    return currencyFormatter.string(from: number)?
+      .replacingOccurrences(of: dashSign, with: negativeSign)
+  }
+
+  // MARK: - Override -
+
+  /// See `NumberFormatter.locale` docs.
+  public var locale: Locale! {
+    get { currencyFormatter.locale }
+    set { currencyFormatter.locale = newValue }
+  }
+
+  /// See `NumberFormatter.allowsFloats` docs.
+  public var allowsFloats: Bool {
+    get { currencyFormatter.allowsFloats }
+    set { currencyFormatter.allowsFloats = newValue }
+  }
+
+  /// See `NumberFormatter.decimalSeparator` docs.
+  public var decimalSeparator: String! {
+    get { currencyFormatter.decimalSeparator }
+    set { currencyFormatter.decimalSeparator = newValue }
+  }
+
+  /// See `NumberFormatter.alwaysShowsDecimalSeparator` docs.
+  public var alwaysShowsDecimalSeparator: Bool {
+    get { currencyFormatter.alwaysShowsDecimalSeparator }
+    set { currencyFormatter.alwaysShowsDecimalSeparator = newValue }
+  }
+
+  /// See `NumberFormatter.currencyDecimalSeparator` docs.
+  public var currencyDecimalSeparator: String! {
+    get { currencyFormatter.currencyDecimalSeparator }
+    set { currencyFormatter.currencyDecimalSeparator = newValue }
+  }
+
+  /// See `NumberFormatter.usesGroupingSeparator` docs.
+  public var usesGroupingSeparator: Bool {
+    get { currencyFormatter.usesGroupingSeparator }
+    set { currencyFormatter.usesGroupingSeparator = newValue }
+  }
+
+  /// See `NumberFormatter.groupingSeparator` docs.
+  public var groupingSeparator: String! {
+    get { currencyFormatter.groupingSeparator }
+    set { currencyFormatter.groupingSeparator = newValue }
+  }
+
+  /// See `NumberFormatter.currencyCode` docs.
+  public var currencyCode: String! {
+    get { currencyFormatter.currencyCode }
+    set { currencyFormatter.currencyCode = newValue }
+  }
+
+  /// See `NumberFormatter.currencySymbol` docs.
+  public var currencySymbol: String! {
+    get { currencyFormatter.currencySymbol }
+    set { currencyFormatter.currencySymbol = newValue }
+  }
+
+  /// See `NumberFormatter.groupingSize` docs.
+  public var groupingSize: Int {
+    get { currencyFormatter.groupingSize }
+    set { currencyFormatter.groupingSize = newValue }
+  }
+
+  /// See `NumberFormatter.secondaryGroupingSize` docs.
+  public var secondaryGroupingSize: Int {
+    get { currencyFormatter.secondaryGroupingSize }
+    set { currencyFormatter.secondaryGroupingSize = newValue }
+  }
+
+  /// See `NumberFormatter.formatWidth` docs.
+  public var formatWidth: Int {
+    get { currencyFormatter.formatWidth }
+    set { currencyFormatter.formatWidth = newValue }
+  }
+
+  /// See `NumberFormatter.paddingCharacter` docs.
+  public var paddingCharacter: String! {
+    get { currencyFormatter.paddingCharacter }
+    set { currencyFormatter.paddingCharacter = newValue }
+  }
+
+  /// See `NumberFormatter.paddingPosition` docs.
+  public var paddingPosition: NumberFormatter.PadPosition {
+    get { currencyFormatter.paddingPosition }
+    set { currencyFormatter.paddingPosition = newValue }
+  }
+
+  /// See `NumberFormatter.roundingMode` docs.
+  public var roundingMode: NumberFormatter.RoundingMode {
+    get { currencyFormatter.roundingMode }
+    set { currencyFormatter.roundingMode = newValue }
+  }
+
+  /// See `NumberFormatter.minimumIntegerDigits` docs.
+  public var minimumIntegerDigits: Int {
+    get { currencyFormatter.minimumIntegerDigits }
+    set { currencyFormatter.minimumIntegerDigits = newValue }
+  }
+
+  /// See `NumberFormatter.maximumIntegerDigits` docs.
+  public var maximumIntegerDigits: Int {
+    get { currencyFormatter.maximumIntegerDigits }
+    set { currencyFormatter.maximumIntegerDigits = newValue }
+  }
+
+  /// See `NumberFormatter.minimumFractionDigits` docs.
+  public var minimumFractionDigits: Int {
+    get { currencyFormatter.minimumFractionDigits }
+    set { currencyFormatter.minimumFractionDigits = newValue }
+  }
+
+  /// See `NumberFormatter.maximumFractionDigits` docs.
+  public var maximumFractionDigits: Int {
+    get { currencyFormatter.maximumFractionDigits }
+    set { currencyFormatter.maximumFractionDigits = newValue }
+  }
+
+  /// See `NumberFormatter.currencyGroupingSeparator` docs.
+  public var currencyGroupingSeparator: String! {
+    get { currencyFormatter.currencyGroupingSeparator }
+    set { currencyFormatter.currencyGroupingSeparator = newValue }
+  }
+
+  /// See `NumberFormatter.usesSignificantDigits` docs.
+  public var usesSignificantDigits: Bool {
+    get { currencyFormatter.usesSignificantDigits }
+    set { currencyFormatter.usesSignificantDigits = newValue }
+  }
+
+  /// See `NumberFormatter.minimumSignificantDigits` docs.
+  public var minimumSignificantDigits: Int {
+    get { currencyFormatter.minimumSignificantDigits }
+    set { currencyFormatter.minimumSignificantDigits = newValue }
+  }
+
+  /// See `NumberFormatter.maximumSignificantDigits` docs.
+  public var maximumSignificantDigits: Int {
+    get { currencyFormatter.maximumSignificantDigits }
+    set { currencyFormatter.maximumSignificantDigits = newValue }
+  }
+
+}
+
+/// This is not designed to extend.
+private class CurrencyNumberFormatter: NumberFormatter {
+
+  override init() {
     super.init()
 
     numberStyle = .currency
@@ -40,15 +214,4 @@ public class PositiveCurrencyFormatter: NumberFormatter {
     fatalError("init(coder:) has not been implemented")
   }
 
-  public override func string(from number: NSNumber) -> String? {
-    if number.doubleValue >= 0 {
-      var value = number.doubleValue
-      value.negate()
-      return super.string(from: NSNumber(value: value))?
-        .replacingOccurrences(of: dashSign, with: positiveSign)
-        .replacingOccurrences(of: negativeSign, with: positiveSign)
-    }
-    return super.string(from: number)?
-      .replacingOccurrences(of: dashSign, with: negativeSign)
-  }
 }
--- Tests/PositiveCurrencyFormatterTests/PositiveCurrencyFormatterTests.swift
+++ Tests/PositiveCurrencyFormatterTests/PositiveCurrencyFormatterTests.swift
@@ -8,63 +8,95 @@ private let nbsp = " ",
 
 final class PositiveCurrencyFormatterTests: XCTestCase {
 
-  func testNlNL_EUR() {
-    let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "nl_NL"), currencyCode: "EUR", currencySymbol: "€")
+  func test_zeroes() {
+    let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "nl_NL"))
 
-    XCTAssertEqual(nf.string(from: -123.65)!, "€\(nbsp)−123,65")
-    XCTAssertEqual(nf.string(from:  123.65)!, "€\(nbsp)+123,65")
+    XCTAssertEqual(nf.string(from: -0.01)!, "€\(nbsp)−0,01")
+    XCTAssertEqual(nf.string(from:  0.01)!, "€\(nbsp)+0,01")
+
+    XCTAssertEqual(nf.string(from: -0)!, "€\(nbsp)0,00")
+    XCTAssertEqual(nf.string(from:  0)!, "€\(nbsp)0,00")
   }
 
-  func testEnUS_USD() {
-    let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "en_US"), currencyCode: "USD", currencySymbol: "$")
+  func test_nl_NL_EUR_nil() {
+    let nf1 = PositiveCurrencyFormatter(locale: Locale(identifier: "nl_NL"), currencyCode: "EUR", currencySymbol: "€")
+    let nf2 = PositiveCurrencyFormatter(locale: Locale(identifier: "nl_NL"), currencyCode: nil, currencySymbol: nil)
+    let nf3 = PositiveCurrencyFormatter(locale: Locale(identifier: "nl_NL"))
 
-    XCTAssertEqual(nf.string(from: -123.65)!, "−$123.65")
-    XCTAssertEqual(nf.string(from:  123.65)!, "+$123.65")
+    XCTAssertEqual(nf1.string(from: -123.65)!, "€\(nbsp)−123,65")
+    XCTAssertEqual(nf1.string(from:  123.65)!, "€\(nbsp)+123,65")
+
+    XCTAssertEqual(nf2.string(from: -123.65)!, "€\(nbsp)−123,65")
+    XCTAssertEqual(nf2.string(from:  123.65)!, "€\(nbsp)+123,65")
+
+    XCTAssertEqual(nf3.string(from: -123.65)!, "€\(nbsp)−123,65")
+    XCTAssertEqual(nf3.string(from:  123.65)!, "€\(nbsp)+123,65")
+
+    XCTAssertEqual(nf1.string(from: -123.65)!, nf2.string(from: -123.65)!, nf3.string(from: -123.65)!)
+    XCTAssertEqual(nf1.string(from:  123.65)!, nf2.string(from:  123.65)!, nf3.string(from:  123.65)!)
   }
 
-  func testEnUS_AED() {
+  func test_en_US_USD_nil() {
+    let nf1 = PositiveCurrencyFormatter(locale: Locale(identifier: "en_US"), currencyCode: "USD", currencySymbol: "$")
+    let nf2 = PositiveCurrencyFormatter(locale: Locale(identifier: "en_US"), currencyCode: nil, currencySymbol: nil)
+    let nf3 = PositiveCurrencyFormatter(locale: Locale(identifier: "en_US"))
+
+    XCTAssertEqual(nf1.string(from: -123.65)!, "−$123.65")
+    XCTAssertEqual(nf1.string(from:  123.65)!, "+$123.65")
+
+    XCTAssertEqual(nf2.string(from: -123.65)!, "−$123.65")
+    XCTAssertEqual(nf2.string(from:  123.65)!, "+$123.65")
+
+    XCTAssertEqual(nf3.string(from: -123.65)!, "−$123.65")
+    XCTAssertEqual(nf3.string(from:  123.65)!, "+$123.65")
+
+    XCTAssertEqual(nf1.string(from: -123.65)!, nf2.string(from: -123.65)!, nf3.string(from: -123.65)!)
+    XCTAssertEqual(nf1.string(from:  123.65)!, nf2.string(from:  123.65)!, nf3.string(from:  123.65)!)
+  }
+
+  func test_en_US_AED() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "en_US"), currencyCode: "AED")
 
     XCTAssertEqual(nf.string(from: -123.65)!, "\(negativeSign)AED\(nbsp)123.65") // -AED 123.65
     XCTAssertEqual(nf.string(from:  123.65)!, "\(positiveSign)AED\(nbsp)123.65") // +AED 123.65
   }
 
-  func testEnUS_ILS() {
+  func test_en_US_ILS() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "en_US"), currencyCode: "ILS")
 
     XCTAssertEqual(nf.string(from: -123.65)!, "\(negativeSign)\(ILS)123.65") // -₪123.65
     XCTAssertEqual(nf.string(from:  123.65)!, "\(positiveSign)\(ILS)123.65") // +₪123.65
   }
 
-  func testHeIL_ILS() {
+  func test_he_IL_ILS() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "he_IL"), currencyCode: "ILS")
 
     XCTAssertEqual(nf.string(from: -123.65)!, "\(rtlMark)\(ltrMark)\(negativeSign)123.65\(nbsp)\(ILS)") // -123.65 ₪
     XCTAssertEqual(nf.string(from:  123.65)!, "\(rtlMark)\(ltrMark)\(positiveSign)123.65\(nbsp)\(ILS)") // +123.65 ₪
   }
 
-  func testHeIL_USD() {
+  func test_he_IL_USD() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "he_IL"), currencyCode: "USD", currencySymbol: "$")
 
     XCTAssertEqual(nf.string(from: -123.65)!, "\(rtlMark)\(ltrMark)\(negativeSign)123.65\(nbsp)$") // -123.65 $
     XCTAssertEqual(nf.string(from:  123.65)!, "\(rtlMark)\(ltrMark)\(positiveSign)123.65\(nbsp)$") // +123.65 $
   }
 
-  func testRU() {
+  func test_ru_RU_RUB() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "ru_RU"), currencyCode: "RUB")
 
     XCTAssertEqual(nf.string(from: -123.65)!, "−123,65\(nbsp)₽")
     XCTAssertEqual(nf.string(from:  123.65)!, "+123,65\(nbsp)₽")
   }
 
-  func testRuRU_USD() {
+  func test_ru_RU_USD() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "ru_RU"), currencyCode: "USD", currencySymbol: "$")
 
     XCTAssertEqual(nf.string(from: -123.65)!, "−123,65\(nbsp)$")
     XCTAssertEqual(nf.string(from:  123.65)!, "+123,65\(nbsp)$")
   }
 
-  func testRuRU_HKD() {
+  func test_ru_RU_HKD() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "ru_RU"), currencyCode: "HKD")
 
     XCTAssertEqual(nf.string(from: -123.65)!, "−123,65\(nbsp)HK$")
@@ -73,21 +105,28 @@ final class PositiveCurrencyFormatterTests: XCTestCase {
 
   // MARK: - English app UI - RTL currencies -
 
-  func testArAE_AED() {
+  func test_ar_AE() {
+    let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "ar_AE"))
+
+    XCTAssertEqual(nf.string(from: -123.65)!, "\(ltrMark)\(negativeSign)\(AED)\(rtlMark)\(nbsp)123.65") // -123 \aed
+    XCTAssertEqual(nf.string(from:  123.65)!, "\(ltrMark)\(positiveSign)\(AED)\(rtlMark)\(nbsp)123.65") // +123 \aed
+  }
+
+  func test_ar_AE_AED() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "ar_AE"), currencyCode: "AED")
 
     XCTAssertEqual(nf.string(from: -123.65)!, "\(ltrMark)\(negativeSign)\(AED)\(rtlMark)\(nbsp)123.65") // -123 \aed
     XCTAssertEqual(nf.string(from:  123.65)!, "\(ltrMark)\(positiveSign)\(AED)\(rtlMark)\(nbsp)123.65") // +123 \aed
   }
 
-  func testArAE_AED_AED() {
+  func test_ar_AE_AED_AED() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "ar_AE"), currencyCode: "AED", currencySymbol: "AED")
 
-    XCTAssertEqual(nf.string(from: -123.65)!, "\(negativeSign)\(AED)\(nbsp)123.65") // -AED 123
-    XCTAssertEqual(nf.string(from:  123.65)!, "\(positiveSign)\(AED)\(nbsp)123.65") // +AED 123 
+    XCTAssertEqual(nf.string(from: -123.65)!, "\(ltrMark)\(negativeSign)AED\(nbsp)123.65") // -AED 123
+    XCTAssertEqual(nf.string(from:  123.65)!, "\(ltrMark)\(positiveSign)AED\(nbsp)123.65") // +AED 123
   }
 
-  func testArAE_USD() {
+  func test_ar_AE_USD() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "ar_AE"), currencyCode: "USD", currencySymbol: "$")
 
     XCTAssertEqual(nf.string(from: -123.65)!, "\(ltrMark)−$\(nbsp)123.65") // -$ 123
@@ -98,7 +137,7 @@ final class PositiveCurrencyFormatterTests: XCTestCase {
   }
 
 
-  func testEnQA_AED() {
+  func test_en_QA_AED() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "en_QA"), currencyCode: "AED")
 
     XCTAssertEqual(nf.string(from: -123.65)!, "\(negativeSign)AED\(nbsp)123.65") // -AED 123
@@ -107,7 +146,7 @@ final class PositiveCurrencyFormatterTests: XCTestCase {
 
   // MARK: - Arabic app UI -
 
-  func testArQA_AED() {
+  func test_ar_QA_AED() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "ar_QA"), currencyCode: "AED")
     let arabic_123_65 = "١٢٣٫٦٥"
 
@@ -115,7 +154,7 @@ final class PositiveCurrencyFormatterTests: XCTestCase {
     XCTAssertEqual(nf.string(from:  123.65)!, "\(u061c)\(positiveSign)\(arabic_123_65)\(nbsp)\(AED)\(rtlMark)")
   }
 
-  func testArQA_QAR_QAR() {
+  func test_ar_QA_QAR_QAR() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "ar_QA"), currencyCode: "QAR", currencySymbol: "QAR")
     let arabic_123_65 = "١٢٣٫٦٥"
 
@@ -123,7 +162,7 @@ final class PositiveCurrencyFormatterTests: XCTestCase {
     XCTAssertEqual(nf.string(from:  123.65)!, "\(u061c)\(positiveSign)\(arabic_123_65)\(nbsp)QAR") // ؜+١٢٣٫٦٥ ر.ق.‏
   }
 
-  func testArQA_USD() {
+  func test_ar_QA_USD() {
     let nf = PositiveCurrencyFormatter(locale: Locale(identifier: "ar_QA"), currencyCode: "USD")
     let arabic_123_65 = "١٢٣٫٦٥"
 
